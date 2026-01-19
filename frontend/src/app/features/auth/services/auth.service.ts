628952246da269ed82b38aa11e621156760857dc
Wiktor <wiko700@gmail.com>
2026-01-19 21:03:00 +0100

    refactor: bump eslint version and change auth service path

--- frontend/src/app/features/auth/services/auth.service.ts
+++ frontend/src/app/features/auth/services/auth.service.ts
@@ -1,15 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RegisterRequest, RegisterResponse } from '../models/register.model';
 import { LoginRequest, LoginResponse } from '../models/login.model';
 import { LogoutResponse } from '../models/logout.model';
+import { RegisterRequest, RegisterResponse } from '../models/register.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private readonly API_URL = 'http://localhost:8001/api/user';
+  private readonly API_URL = 'http://localhost:8001/api/auth';
   private readonly http = inject(HttpClient);
 
   register(request: RegisterRequest): Observable<RegisterResponse> {
